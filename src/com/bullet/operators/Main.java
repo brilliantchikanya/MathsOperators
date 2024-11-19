928d38b5f41646a754b59145799d0f2fb053bb41
brilliantchikanya <brilliant.chikanya@gmail.com>
2024-11-19 07:52:29 +0200

    updated the files

--- src/com/bullet/operators/Main.java
+++ src/com/bullet/operators/Main.java
@@ -2,7 +2,7 @@ package com.bullet.operators;
 
 import java.util.Random;
 
-import static com.bullet.operators.Functions.printVariables;
+import static com.bullet.operators.UtilFunctions.printVariables;
 
 /**
  * @author brilliant
@@ -13,13 +13,13 @@ import static com.bullet.operators.Functions.printVariables;
  * Some of them include '+', '-', '*', '/', among others*/
 public class Main {
     public static void main(String[] args) {
+        // create a number generator
         Random generator = new Random(47);
 
         // integer number examples
-        int x = generator.nextInt(100) + 1;
-        //System.out.println(x);
-        int y = generator.nextInt(100) + 1;
-        //System.out.println(y);
+        int x, y, z;    // declare three variables:
+        x = generator.nextInt(100) + 1; // obtain an integer between 0 and 100
+        y = generator.nextInt(100) + 1; // we offset by 1 since we will use division
         printVariables(x, y);
         int difference = x - y;
         System.out.println(difference);
--- src/com/bullet/operators/UtilFunctions.java
+++ src/com/bullet/operators/UtilFunctions.java
@@ -5,8 +5,14 @@ package com.bullet.operators;
  * @author com.bullet
  * @since 18-11-2024*/
 
-public class Functions {
+public class UtilFunctions {
+
+
+    // this method presents formatted int variables and their values
     static void printVariables(int x, int y) {
+        print(x, y);
+    }
+    static void print(int x, int y) {
         System.out.println("*************************************");
         System.out.print(" variable     | ");
         System.out.print("   x   |  ");
@@ -15,5 +21,9 @@ public class Functions {
         System.out.print("  " + x + "  |   ");
         System.out.println("  " + y + "   |");
         System.out.println("*************************************");
+    }
+
+    static void add(int x, int y) {
+        //
     }
 }
